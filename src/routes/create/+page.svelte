5fc18ffb27df7893e72688968a878079301c7b1b
nagneo <tosk3927@gmail.com>
2024-03-01 16:12:28 +0900

    initial commit

---
+++ src/routes/create/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Create Fridge</h1>
+<!-- Inlined classes -->
+<div class="bg-primary-50 text-primary-900">Skeleton</div>
+<!-- Tailwind opacity scale -->
+<div class="border border-primary-500/30">Skeleton</div>
---
+++ src/routes/detail/[slug]/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Slug Certain Fridge</h1>
